f7dfa66a3c6bc5b7cf7d6045b8a1700ad273d497
Chu Thin <thincv@live.com>
2019-03-11 15:37:24 +0700

    Commit lib

---
+++ CycleSwift/CycleSwift/CycleSwift.h
@@ -0,0 +1,19 @@
+//
+//  CycleSwift.h
+//  CycleSwift
+//
+//  Created by ptpm on 3/11/19.
+//  Copyright © 2019 thincv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CycleSwift.
+FOUNDATION_EXPORT double CycleSwiftVersionNumber;
+
+//! Project version string for CycleSwift.
+FOUNDATION_EXPORT const unsigned char CycleSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CycleSwift/PublicHeader.h>
+
+
---
+++ CycleSwift/CycleSwift/Service.swift
@@ -0,0 +1,14 @@
+//
+//  Service.swift
+//  CycleSwift
+//
+//  Created by ptpm on 3/11/19.
+//  Copyright © 2019 thincv. All rights reserved.
+//
+
+import Foundation
+public class Service {
+    public static func doSomething() -> String {
+        return "Do something!"
+    }
+}
